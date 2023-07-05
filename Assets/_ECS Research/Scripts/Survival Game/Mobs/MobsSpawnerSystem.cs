a0ae9de05bb09938d81f8561b4e520e689fd64ae
Trương Hoàng Hiệp <hiepht@tqcglobal.com>
2023-07-05 18:23:44 +0700

    + Util random gen + Backup commit

--- Assets/_ECS Research/Scripts/Survival Game/Mobs/MobsSpawnerSystem.cs
+++ Assets/_ECS Research/Scripts/Survival Game/Mobs/MobsSpawnerSystem.cs
@@ -1,12 +1,50 @@
+using Unity.Burst;
 using Unity.Entities;
-using UnityEngine;
-
 
 
 namespace _ECS_Research.Scripts.Survival_Game.Mobs
 {
-    public partial class MobsSpawnerSystem : ISystem
+    public partial struct MobsSpawnerSystem : ISystem
     {
-    
+        #region ISystem Callbacks
+
+        [BurstCompile] public void OnCreate(ref SystemState _state)
+        {
+            _state.RequireForUpdate<BeginSimulationEntityCommandBufferSystem.Singleton>();
+        }
+
+
+        [BurstCompile] public void OnUpdate(ref SystemState _state)
+        {
+            var ecb = GetEntityCommandBuffer(ref _state);
+
+            new SpawnMobsJob {ecb = ecb}.ScheduleParallel();
+        }
+
+
+
+        [BurstCompile] public partial struct SpawnMobsJob : IJobEntity
+        {
+            public EntityCommandBuffer.ParallelWriter ecb;
+
+
+            private void Execute()
+            {
+            }
+        }
+
+        #endregion
+
+
+        #region Utils
+
+        private EntityCommandBuffer.ParallelWriter GetEntityCommandBuffer(ref SystemState _state)
+        {
+            var ecbSingleton = SystemAPI.GetSingleton<BeginSimulationEntityCommandBufferSystem.Singleton>();
+            var ecb = ecbSingleton.CreateCommandBuffer(_state.WorldUnmanaged);
+            return ecb.AsParallelWriter();
+        }
+
+        #endregion
     }
 }
--- Assets/_ECS Research/Scripts/Utils.cs
+++ Assets/_ECS Research/Scripts/Utils.cs
@@ -5,9 +5,30 @@ namespace _ECS_Research.Scripts
 {
     public static class Utils
     {
-        public static float3 GetRandomPosition(ref Unity.Mathematics.Random _rdm, float2 _xBounds, float2 _yBounds, float _depth = 0f)
+        /// <summary>
+        /// Random pos in UI coordinate 
+        /// </summary>
+        public static float3 GetRandomPosition(ref Random _rdm, float2 _xBounds, float2 _yBounds, float _depth = 0f)
         {
             return new float3(_rdm.NextFloat(_xBounds.x, _xBounds.y), _rdm.NextFloat(_yBounds.x, _yBounds.y), _depth);
+        }
+
+
+        /// <summary>
+        /// Random pos in platform coordinate with offset condition 
+        /// </summary>
+        public static float3 GetRandomPosition(ref Random _rdm, float2 _offsetRange, float _height)
+        {
+            var rawRand = _rdm.NextFloat2(-_offsetRange.y, _offsetRange.y);
+            var rawLength = math.length(rawRand);
+
+            while (rawLength < _offsetRange.x || rawLength > _offsetRange.y)
+            {
+                rawRand = _rdm.NextFloat2(-_offsetRange.y, _offsetRange.y);
+            }
+
+            var result = new float3(rawRand.x, _height, rawRand.y);
+            return result;
         }
     }
 }
